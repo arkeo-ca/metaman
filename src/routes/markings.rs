9e78f8a9eb7ae7666cde4bdbf37982b75985c7d3
Francis Holtken <dev@arkeo.ca>
2022-07-06 15:13:51 -0300

    Add logic for creating markings

--- src/routes/markings.rs
+++ src/routes/markings.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct MarkingData {
@@ -7,6 +10,26 @@ pub struct MarkingData {
     definition: String,
 }
 
-pub async fn create_marking(_form: web::Json<MarkingData>) -> HttpResponse {
-    HttpResponse::Created().finish()
+pub async fn create_marking(form: web::Json<MarkingData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO markings (id, name, definition_type, definition, created_at, created_by)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        "#,
+        Uuid::new_v4(),
+        form.name,
+        form.definition_type,
+        form.definition,
+        Utc::now(),
+        Uuid::new_v4()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Created().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,24 +1,39 @@
 use metaman::configuration::get_configuration;
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-fn spawn_app() -> String {
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = metaman::startup::run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let server =
+        metaman::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    let app_address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{}/health_check", &app_address))
+        .get(format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -29,17 +44,12 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn create_marking_returns_a_201_for_valid_form_data() {
-    let app_address = spawn_app();
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres.");
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let body = "{\"name\": \"tlp_red\", \"definition_type\": \"tlp\", \"definition\": \"TLP Red\"}";
     let response = client
-        .post(&format!("{}/markings", &app_address))
+        .post(&format!("{}/markings", &app.address))
         .header("Content-Type", "application/json")
         .body(body)
         .send()
@@ -49,7 +59,7 @@ async fn create_marking_returns_a_201_for_valid_form_data() {
     assert_eq!(201, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT name, definition_type, definition FROM markings",)
-        .fetch_one(&mut connection)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved marking.");
 
@@ -60,7 +70,7 @@ async fn create_marking_returns_a_201_for_valid_form_data() {
 
 #[tokio::test]
 async fn create_marking_returns_a_400_when_data_is_missing() {
-    let app_address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
@@ -93,7 +103,7 @@ async fn create_marking_returns_a_400_when_data_is_missing() {
 
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(&format!("{}/markings", &app_address))
+            .post(&format!("{}/markings", &app.address))
             .header("Content-Type", "application/json")
             .body(invalid_body)
             .send()
