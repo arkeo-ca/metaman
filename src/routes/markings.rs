859f94d2a856d062129d3ce88cdecfcfcae490a3
Francis Holtken <dev@arkeo.ca>
2022-07-06 22:40:22 -0300

    Add logging/tracing capabilities

--- src/routes/markings.rs
+++ src/routes/markings.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,6 +12,21 @@ pub struct MarkingData {
 }
 
 pub async fn create_marking(form: web::Json<MarkingData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new marking.",
+        %request_id,
+        marking_name = form.name,
+        marking_type = form.definition_type,
+        marking_definition = form.definition
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new marking in the database.");
+
+    tracing::info!(
+        "request_id {} - Creating new marking in database.",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO markings (id, name, definition_type, definition, created_at, created_by)
@@ -24,11 +40,12 @@ pub async fn create_marking(form: web::Json<MarkingData>, pool: web::Data<PgPool
         Uuid::new_v4()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => HttpResponse::Created().finish(),
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
